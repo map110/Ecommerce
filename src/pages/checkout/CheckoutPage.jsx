85115d9443b71c2ac0116a39507fe34ec20b0a4d
m.peykanfar <m.peykanfar@PEYKANFAR-PC>
2026-05-21 15:26:51 +0330

    امکان آپدیت delivery option

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -5,7 +5,7 @@ import "./checkout-header.css";
 import { OrderSummary } from "./OrderSummary";
 import { PaymentSummary } from "./PaymentSummary";
 
-export function CheckoutPage({ cart }) {
+export function CheckoutPage({ cart, loadCart }) {
   const [deliveryOptions, setDeliveryOptions] = useState([]);
   const [paymentSummary, setPaymentSummary] = useState(null);
 
@@ -20,7 +20,7 @@ export function CheckoutPage({ cart }) {
       setPaymentSummary(response.data);
     };
     fetchHomeData();
-  }, []);
+  }, [cart]);
   return (
     <>
       <title>Checkout</title>
@@ -51,7 +51,11 @@ export function CheckoutPage({ cart }) {
         <div className="page-title">Review your order</div>
 
         <div className="checkout-grid">
-          <OrderSummary cart={cart} deliveryOptions={deliveryOptions} />
+          <OrderSummary
+            cart={cart}
+            deliveryOptions={deliveryOptions}
+            loadCart={loadCart}
+          />
           <PaymentSummary paymentSummary={paymentSummary} />
         </div>
       </div>
--- src/pages/checkout/OrderSummary.jsx
+++ src/pages/checkout/OrderSummary.jsx
@@ -2,7 +2,7 @@ import dayjs from "dayjs";
 import { formatMoney } from "../../utils/money";
 import { DeliveryOptions } from "./DeliveryOptions";
 
-export function OrderSummary({ cart, deliveryOptions }) {
+export function OrderSummary({ cart, deliveryOptions, loadCart }) {
   return (
     <div className="order-summary">
       {cart.map((cartItem) => {
@@ -46,6 +46,7 @@ export function OrderSummary({ cart, deliveryOptions }) {
               <DeliveryOptions
                 cartItem={cartItem}
                 deliveryOptions={deliveryOptions}
+                loadCart={loadCart}
               />
             </div>
           </div>
